5eb422f668a8443488c4d0e65e5275c75ca1dd75
Kirk Dirk <jackalex@ya.ru>
2022-09-15 23:09:23 +0500

    code: exclusion of long elements from the starting array

--- Program.cs
+++ Program.cs
@@ -1,10 +1,10 @@
 ﻿// Код для выборки из массива строк элементов, 
 // количество символов которых меньше или равно трём
 
-void PrintArray(string[] array)
+// Метод для печати массива
+void PrintArray(string[] array, string name)
 {
-    Console.Clear();
-    Console.WriteLine("Заданный массив строк:");
+    Console.WriteLine($"{name} массив строк:");
     for (int i = 0; i < array.GetLength(0); i++)
         Console.WriteLine($"{i}-й элемент: {array[i]}");
 }
@@ -14,12 +14,44 @@ Console.Clear();
 Console.WriteLine("Введите размер массива строк - целое положительное число:");
 int startLength = Convert.ToInt32(Console.ReadLine());
 string[] array0 = new string[startLength];
-int i = 0;
-while (i < startLength)
+int x = 0;
+while (x < startLength)
 {
-    Console.WriteLine($"Введите элемент {i}-й массива - строку:");
-    array0[i] = Console.ReadLine();
-    i++;
+    Console.WriteLine($"Введите элемент {x}-й массива - строку:");
+    array0[x] = Console.ReadLine();
+    x++;
 }
-PrintArray(array0);
+Console.Clear();
+PrintArray(array0, "Заданный");
 
+//Проверяем длину значений массива 
+// и записываем значения во второй массив длиной не больше 4 символов 
+// или оставляем Emty, если элемент длиннее. Подсчитываем количество 
+// ненулевых элементов. Печатаем промежуточный массив
+string[] array1 = new string[startLength];
+int numberShort = startLength;
+for (int i = 0; i < startLength; i++)
+{
+    if (array0[i].Length < 4) array1[i] = array0[i];
+    else
+    {
+        array1[i] = string.Empty;
+        numberShort--;
+    }
+}
+//PrintArray(array1, "Промежуточный");
+
+// Cоздаем третий массив соответствующего размера. 
+// Заполняем третий массив ненулевыми значениями 
+// из второго массива. Выводим результат
+string[] array2 = new string[numberShort];
+int y = 0;
+for (int i = 0; i < startLength; i++)
+{
+    if (array1[i] != string.Empty)
+    {
+        array2[y] = array1[i];
+        y++;
+    }
+}
+PrintArray(array2, "Итоговый");
